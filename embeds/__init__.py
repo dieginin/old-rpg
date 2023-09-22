b899db89093246ee4c6f5cf5acde3e4a70d8f5b0
dieginin <diegobalestra9@gmail.com>
2023-09-22 15:21:37 -0400

    feat: create embeds/__init__.py, inventory_embed.py and status_embed.py

---
+++ embeds/__init__.py
@@ -0,0 +1,2 @@
+from embeds.inventory_embed import *
+from embeds.status_embed import *
---
+++ embeds/inventory_embed.py
@@ -0,0 +1,21 @@
+import discord
+
+from game import Character
+
+
+def inventory_embed(ctx, character: Character):
+    # Create embed with invenory description
+    embed = discord.Embed(
+        title=f"**{character.name}** Inventory",
+        color=discord.Color.random(),
+    )
+    embed.set_author(name=ctx.author.display_name)
+
+    # Inventory field
+    inventory_text = f"Gold: {character.gold}\n"
+    if character.inventory:
+        inventory_text += "\n".join(character.inventory)
+
+    embed.add_field(name="Inventory", value=inventory_text)
+
+    return embed
