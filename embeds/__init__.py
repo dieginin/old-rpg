7ce898bb49ad5e7be8d706fa5e37ec7981498a7a
dieginin <diegobalestra9@gmail.com>
2023-09-22 12:05:17 -0400

    embeds a su carpeta

---
+++ embeds/__init__.py
@@ -0,0 +1,2 @@
+from embeds.inventary_embed import *
+from embeds.status_embed import *
---
+++ embeds/status_embed.py
@@ -0,0 +1,50 @@
+import discord
+
+from helpers import GameMode
+
+MODE_COLOR = {
+    GameMode.BATTLE: 0xDC143C,
+    GameMode.ADVENTURE: 0x005EB8,
+}
+
+
+def status_embed(ctx, character):
+    mode_text = ""
+
+    # Current mode
+    if character.mode == GameMode.BATTLE:
+        mode_text = f"Currently battling a {character.battling.name}."
+    elif character.mode == GameMode.ADVENTURE:
+        mode_text = "Currently adventuring."
+
+    # Create embed with description as current mode
+    embed = discord.Embed(
+        title=f"**{character.name}** status",
+        description=mode_text,
+        color=MODE_COLOR[character.mode],
+    )
+    embed.set_author(name=ctx.author.display_name)
+
+    # Stats field
+    _, xp_needed = character.ready_to_level_up()
+
+    embed.add_field(
+        name="Stats",
+        value=f"""
+**HP     :**    {character.hp}/{character.max_hp}
+**ATTACK :**   {character.attack}
+**DEFENSE:**   {character.defense}
+**MANA:**  {character.mana}
+**LEVEL:** {character.level}
+**XP:**    {character.xp}/{character.xp+xp_needed}
+    """,
+    )
+
+    # Inventory field
+    inventory_text = f"Gold: {character.gold}\n"
+    if character.inventory:
+        inventory_text += "\n".join(character.inventory)
+
+    embed.add_field(name="Inventory", value=inventory_text)
+
+    return embed
