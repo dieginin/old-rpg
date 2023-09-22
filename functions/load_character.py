ae2ba21c203ee49c295ec370a802bb364208921f
dieginin <diegobalestra9@gmail.com>
2023-09-22 16:59:03 -0400

    update load_character.py

--- functions/load_character.py
+++ functions/load_character.py
@@ -2,5 +2,5 @@ from connection import characters
 from models import Character
 
 
-def load_character(user_id: str) -> Character:
+def load_character(user_id: int) -> Character:
     return Character(**list(characters.find({"_id": user_id}))[0]["character"])
