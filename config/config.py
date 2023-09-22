6b4dca03ca3d50d98d84211e20865ebc4e6a4316
dieginin <diegobalestra9@gmail.com>
2023-09-22 13:55:02 -0400

    create 1 file and move 1 file

--- config/config.py
+++ config/config.py
@@ -2,4 +2,7 @@ from dotenv import dotenv_values
 
 dot = dotenv_values()
 DISCORD_TOKEN = dot["DISCORD_TOKEN"]
+
+DB_CLUSTER = dot["DB_CLUSTER"]
 DB_PASSWORD = dot["DB_PASSWORD"]
+DB_USER = dot["DB_USER"]
